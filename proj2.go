08d6d409149f1c015b9204c4c53d9008b0190240
Alex <alexrao20002@gmail.com>
2020-11-23 20:43:33 -0800

    update user struct

--- proj2.go
+++ proj2.go
@@ -89,6 +89,8 @@ type User struct {
 	K_private userlib.PKEDecKey
 	K_DS_private userlib.DSSignKey
 	AES_key_storage_keys map[string]uuid.UUID
+	//AES_key_shared_keys map[string]uuid.UUID
+	Direct_recipients map[string][]string
 
 	// You can add other fields here if you want...
 	// Note for JSON to marshal/unmarshal, the fields need to
@@ -341,10 +343,12 @@ func InitUser(username string, password string) (userdataptr *User, err error) {
 
 	// Store user data
 	userdata.Username = username
-	userdata.AES_key_storage_keys = make(map[string]uuid.UUID)
 	userdata.K_password = k_password
 	userdata.K_private = K_private
 	userdata.K_DS_private = K_DS_private
+	userdata.AES_key_storage_keys = make(map[string]uuid.UUID)
+	//userdata.AES_key_shared_keys = make(map[string]uuid.UUID)
+	userdata.Direct_recipients = make(map[string][]string)
 
 	//store public keys
 	k_pubkey, k_DSkey := StorageKeysPublicKey(username)
@@ -452,6 +456,10 @@ func (userdata *User) StoreFile(filename string, data []byte) {
 	const k_password_len uint32 = 16
 	const ENCRYPTED_VOLUME_SIZE = 1048576 /*VOLUME_SIZE*/ + 16 /*userlib.AESBlockSize*/
 	// userlib.DebugMsg("VOLUME_SIZE mod AES block size is %v", VOLUME_SIZE % userlib.AESBlockSize)
+	
+	// Initialize direct recipients
+	var recipients []string
+	userdata.Direct_recipients[filename] = recipients
 
 	// Encoding
 	packaged_data, _ := json.Marshal(data)
@@ -599,6 +607,10 @@ func (userdata *User) ShareFile(filename string, recipient string) (
 	magic_string string, err error) {
 	const k_password_len uint32 = 16
 
+	//Add recipient to direct recipients
+	d_r := userdata.Direct_recipients[filename]
+	userdata.Direct_recipients[filename] = append(d_r, recipient)
+
 	//Retrieve k_file
 	ID_k := userdata.AES_key_storage_keys[filename]
 	k_file, err := GetAESKeys(ID_k, userdata)
@@ -681,6 +693,8 @@ func (userdata *User) ReceiveFile(filename string, sender string,
 	if err != nil {
 		return err
 	}
+
+	userdata.AES_key_storage_keys[filename] = ID_k
 
 	//Add new file to map
 	k_file, err := GetAESKeys(ID_k, userdata)
