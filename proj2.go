de6bac96c0cbbcf371ef4310bc0afb816136c520
Ziyi Lu <lzy-1006@berkeley.edu>
2020-11-24 16:58:31 -0800

    LoadFile complete, buggy

--- proj2.go
+++ proj2.go
@@ -502,98 +502,6 @@ func VerifyAndDecryptVolume(volume_encrypted Volume, index int, n_volumes int, k
 	return volume, pad_last, nil
 }
 
-// Load, verify, & decrypt volumes of FILENAME with USERDATA's credentials
-func LoadVolumes(userdata *User, filename string) (volumes [][]byte, err error) {
-	// Get AES keys
-	ID_k := userdata.AES_key_storage_keys[filename]
-	k_file, err := GetAESKeys(ID_k, userdata)
-	if err != nil {
-		return nil, errors.New(strings.ToTitle("File not found!"))
-	}
-
-	// Get ciphertext
-	hash_ID_k := userlib.Hash([]byte(ID_k.String()))
-	ID_file, err := uuid.FromBytes(hash_ID_k[:16])
-	if err != nil {
-		userlib.DebugMsg("%v", err)
-		return
-	}
-	stored, ok := userlib.DatastoreGet(ID_file)
-	if !ok {
-		return nil, errors.New(strings.ToTitle("File not found!"))
-	}
-	var volumes_encrypted []Volume
-	json.Unmarshal(stored, &volumes_encrypted)
-
-	// Verify & decrypt
-	n_volumes := len(volumes_encrypted)
-	var pad_last uint32
-	for index, volume_encrypted := range volumes_encrypted {
-		volumes[index], pad_last, err = VerifyAndDecryptVolume(volume_encrypted, index, n_volumes, k_file)
-		// pad_last is finalised in last iteration
-		if err != nil {
-			userlib.DebugMsg("%v", err)
-			return nil, err
-		}
-	}
-	return volumes, nil
-}
-
-/* Verify and Decrypt VOLUME_ENCRYPTED at INDEX in the volume array
- that is N_VOLUMES long with key K_FILE */
-func VerifyAndDecryptVolume(volume_encrypted Volume, index int, n_volumes int, k_file []byte) (volumes [][]byte, pad_last uint32, err error) {
-	const VOLUME_SIZE = 1048576 // 2^20 bytes
-	const k_password_len uint32 = 16
-	const ENCRYPTED_VOLUME_SIZE = 1048576 /*VOLUME_SIZE*/ + 16 /*userlib.AESBlockSize*/
-
-	// Check length
-	if len(volume_encrypted.Ciphertext) != ENCRYPTED_VOLUME_SIZE {
-		return nil, 0, errors.New(strings.ToTitle("Wrong ciphertext length"))
-	}
-
-	// Check padding
-	pad_last = 0
-	if index == n_volumes - 1 {
-		pad_last = volume_encrypted.N_pad
-	} else if volume_encrypted.N_pad != 0 {
-		return nil, 0, errors.New(strings.ToTitle("Non-last volume has non-zero padding"))
-	}
-
-	index_string := strconv.Itoa(index)
-
-	// Verify
-	salt_volume_authentication := []byte("volume_authentication" + index_string)
-	k_volume_MAC, err := userlib.HashKDF(k_file,
-		salt_volume_authentication)
-	if err != nil {
-		userlib.DebugMsg("%v", err)
-		return nil, 0, err
-	}
-	k_volume_MAC = k_volume_MAC[:k_password_len]
-	MAC, err := userlib.HMACEval(k_volume_MAC, volume_encrypted.Ciphertext)
-	if err != nil {
-		userlib.DebugMsg("%v", err)
-		return nil, 0, err
-	}
-	if !userlib.HMACEqual(volume_encrypted.MAC, MAC){
-		return nil, 0, errors.New(strings.ToTitle("Verification failed"))
-	}
-
-	// Decrypt
-	salt_volume_encryption := []byte("volume_encryption" + index_string)
-	k_volume, err := userlib.HashKDF(k_file,
-		salt_volume_encryption)
-	if err != nil {
-		userlib.DebugMsg("%v", err)
-		return nil, 0, err
-	}
-	k_volume = k_volume[:k_password_len]
-	defer HandlePanics()
-	volume := userlib.SymDec(k_volume, volume_encrypted.Ciphertext)
-
-	return volume, pad_last, nil
-}
-
 // This handles panics and should print the error
 func HandlePanics()  {
 	if recovery := recover(); recovery != nil {
@@ -894,19 +802,12 @@ func (userdata *User) LoadFile(filename string) (data []byte, err error) {
 		return nil, errors.New(strings.ToTitle("Padding longer than VOLUME_SIZE"))
 	}
 
-<<<<<<< HEAD
 	//err = json.Unmarshal(packaged_data, &data)
 	//if err != nil {
 	//	userlib.DebugMsg("%v", err)
 	//	return nil, err
 	//}
 	return packaged_data, nil
-=======
-	return volumes[0], nil
-	//End of toy implementation
-
-	return
->>>>>>> LoadVolumes
 }
 
 // This creates a sharing record, which is a key pointing to something
@@ -1029,7 +930,6 @@ func (userdata *User) ReceiveFile(filename string, sender string,
 	if err != nil {
 		return err
 	}
-
 	StoreAESKeys(ID_k, k_file, userdata, userdata.Username)
 	*/
 
